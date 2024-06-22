203cc09cdfcc5b65b80e214379aa8a09caf27e5e
sbpirzada <sb.pirzada@yahoo.com>
2024-06-22 11:07:21 +0500

    Pushing my Word Counter App on GitHub.

---
+++ main.js
@@ -0,0 +1,22 @@
+#!/usr/env code
+// Installing inquirer and chalk packages
+import inquirer from "inquirer";
+import chalk from "chalk";
+//Display a colorful Welcome Message
+console.log(chalk.bold.cyanBright("\n \tShah Bano Pirzada - Word Counter\n"));
+console.log("=".repeat(60));
+// Prompt the user to enter a sentence
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter a sentence",
+    }
+]);
+// Trimming the sentence and splitting it into words based on "spaces"
+let words = answers.sentence.trim().split(" ");
+//Analysis of user input sentence
+console.log("=".repeat(60));
+console.log(chalk.bold("- Sentence Words:"));
+console.log(words);
+console.log(chalk.bold(`\n - Word Count: ${words.length}`));
